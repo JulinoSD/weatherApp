426e2354a2f4ccc1261560a58aaa199418cbff36
JulinoSD <ddimojulino@gmail.com>
2021-09-27 11:11:41 -0300

    first step

---
+++ src/components/layout.tsx
@@ -0,0 +1,44 @@
+import { FunctionComponent, ReactNode } from "react";
+import Link from 'next/link'
+
+interface IProps {
+    main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+    const authenticated = false
+
+    const logout = () => {
+        return <div>Parser</div>
+    }
+
+    return (
+        <div className="bg-gray-900 max-w-screen-1xl max-auto">
+            <nav className="bg-gray-800 max-w-screen" style={{ height: '4rem' }}>
+                <div className="p-6 flex items-center justify-between">
+                    <input type="text"
+                        name="search"
+                        id="search"
+                        placeholder="Local/Cidade/Estado"
+                        style={{ width: '250px', borderRadius: '2px' }}
+                    />
+                    {authenticated ? <>
+                        {/* <button onClick={() => { }}>Log out</button> */}
+                        <button onClick={logout}>Log out</button>
+                    </> : <>
+                        <Link href="/auth">
+                            <a>
+                                Registre-se
+                            </a>
+                        </Link>
+                    </>}
+                </div>
+            </nav>
+            <main style={{ minHeight: "calc(100vh - 4rem)" }}>
+                {main}
+            </main>
+        </div>
+    )
+}
+
+export default Layout
