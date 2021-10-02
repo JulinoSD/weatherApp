c199b18d36b5395f04b4a4bceee3d9186a565647
JulinoSD <ddimojulino@gmail.com>
2021-10-02 14:53:55 -0300

    Bug css nav5

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,20 @@ interface WProps {
 const Home: NextPage = () => {
   return <Layout
     main={<div className="flex flex-wrap flex-row justify-center items-center m-5" style={{ minHeight: 'calc(100vh - 4rem)' }}> {/*Main*/}
-      <div className="bg-red-700 rounded w-96 p-1 m-2 h-36">
+      <div className="bg-red-600 rounded w-96 p-1 m-2 h-36">{/*cardWeather*/}
+        <p>Cidade, Estado</p>
+        <p>data, hora</p>
+        <span className="weatherData grid-cols-3 gap-4 auto-cols-max"> {/*weatherData */}
+          <img src="#" alt="temp" />
+          <span>
+            <p>Chuva: 2%</p>
+            <p>Humidade: 68%</p>
+            <p>Vento: 31km/h</p>
+          </span>
+          <img src="#" alt="Estado do tempo" />
+        </span>
+      </div>
+      <div className="bg-red-600 rounded w-96 p-1 m-2 h-36">
         <p>Cidade, Estado</p>
         <p>data, hora</p>
         <span className="weatherData grid-cols-3 gap-4 auto-cols-max">
@@ -22,73 +35,60 @@ const Home: NextPage = () => {
             <p>Humidade: 68%</p>
             <p>Vento: 31km/h</p>
           </span>
-            <img src="#" alt="Estado do tempo" />
+          <img src="#" alt="Estado do tempo" />
         </span>
       </div>
-      <div className="cardWeather bg-red-700 rounded w-22 p-1">
+      <div className="bg-red-600 rounded w-96 p-1 m-2 h-36">
         <p>Cidade, Estado</p>
         <p>data, hora</p>
-        <span className="weatherData">
+        <span className="weatherData grid-cols-3 gap-4 auto-cols-max">
           <img src="#" alt="temp" />
           <span>
             <p>Chuva: 2%</p>
             <p>Humidade: 68%</p>
             <p>Vento: 31km/h</p>
           </span>
+          <img src="#" alt="Estado do tempo" />
         </span>
-        <img src="#" alt="Estado do tempo" />
       </div>
-      <div className="cardWeather bg-red-700 rounded w-22 p-1">
+      <div className="bg-red-600 rounded w-96 p-1 m-2 h-36">
         <p>Cidade, Estado</p>
         <p>data, hora</p>
-        <span className="weatherData">
+        <span className="weatherData grid-cols-3 gap-4 auto-cols-max">
           <img src="#" alt="temp" />
           <span>
             <p>Chuva: 2%</p>
             <p>Humidade: 68%</p>
             <p>Vento: 31km/h</p>
           </span>
+          <img src="#" alt="Estado do tempo" />
         </span>
-        <img src="#" alt="Estado do tempo" />
       </div>
-      <div className="cardWeather bg-red-700 rounded w-22 p-1">
+      <div className="bg-red-600 rounded w-96 p-1 m-2 h-36">
         <p>Cidade, Estado</p>
         <p>data, hora</p>
-        <span className="weatherData">
+        <span className="weatherData grid-cols-3 gap-4 auto-cols-max">
           <img src="#" alt="temp" />
           <span>
             <p>Chuva: 2%</p>
             <p>Humidade: 68%</p>
             <p>Vento: 31km/h</p>
           </span>
+          <img src="#" alt="Estado do tempo" />
         </span>
-        <img src="#" alt="Estado do tempo" />
       </div>
-      <div className="cardWeather bg-red-700 rounded w-22 p-1">
+      <div className="bg-red-600 rounded w-96 p-1 m-2 h-36">
         <p>Cidade, Estado</p>
         <p>data, hora</p>
-        <span className="weatherData">
+        <span className="weatherData grid-cols-3 gap-4 auto-cols-max">
           <img src="#" alt="temp" />
           <span>
             <p>Chuva: 2%</p>
             <p>Humidade: 68%</p>
             <p>Vento: 31km/h</p>
           </span>
+          <img src="#" alt="Estado do tempo" />
         </span>
-        <img src="#" alt="Estado do tempo" />
-      </div>
-      <div className="cardWeather bg-red-700 rounded w-22 p-1">
-        <p style={{ font: '600 10pt/150% Roboto, sans-serif' }}>Cidade, Estado</p>
-        <p>data, hora</p>
-        <span className="weatherData">
-          <img src="#" alt="temp" />
-          <span>
-            <p>Chuva: 2%</p>
-            <p>Humidade: 68%</p>
-            <p>Vento: 31km/h</p>
-          </span>
-        </span>
-        <img src="#" alt="Estado do tempo" />
       </div>
     </div>}
   />
